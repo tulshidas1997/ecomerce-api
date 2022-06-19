4a64859bc5fa0d8e3c9c401de0a14b02b6104634
tulshi das <bsse0811@iit.du.ac.bd>
2022-06-19 16:52:59 +0000

    add add db context

--- Repository/Context/AppDbContext.cs
+++ Repository/Context/AppDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace Repositories.Context;
+﻿using Core.Interfaces;
+using Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext
+namespace Repositories.Context;
+
+public sealed class AppDbContext: IdentityDbContext<AppUser>
 {
-    
+    private readonly ICurrentUserService _currentUserService;
+    public AppDbContext(DbContextOptions<AppDbContext> opt, ICurrentUserService currentUserService):base(opt)
+    {
+        _currentUserService = currentUserService;
+    }
+
+    public DbSet<Cow> Cows { get; set; }
 }
