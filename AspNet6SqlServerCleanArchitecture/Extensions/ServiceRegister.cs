d1f77cd857eaa86d78a3a9e4852e7d3e0550ef41
tulshi das <bsse0811@iit.du.ac.bd>
2022-07-31 11:22:38 +0000

    fix db connection problem

--- AspNet6SqlServerCleanArchitecture/Extensions/ServiceRegister.cs
+++ AspNet6SqlServerCleanArchitecture/Extensions/ServiceRegister.cs
@@ -1,11 +1,7 @@
 ﻿using Core.Interfaces;
 using Core.Interfaces.Repositories;
 using Core.Interfaces.Services;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Repositories;
-using Repositories.Context;
 using Services;
 
 namespace AspNet6SqlServerCleanArchitecture.Extensions;
@@ -26,31 +22,14 @@ public static class ServiceRegister
         services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
         services.AddScoped(typeof(IBaseRepository<,>), typeof(BaseRepository<,>));
         services.AddScoped<ICowRepository,CowRepository>();
-        //services.AddScoped<ITestRepository, TestRepository>();
-
-
-        //services.RegisterAssemblyPublicNonGenericClasses(AppDomain.CurrentDomain.GetAssemblies())
-        //    .Where(c => c.Name.EndsWith("Repository"))
-        //    .AsPublicImplementedInterfaces();
-
-        //services.RegisterAssemblyPublicNonGenericClasses(AppDomain.CurrentDomain.GetAssemblies())
-        //    .Where(c => c.Name.EndsWith("Service"))
-        //    .AsPublicImplementedInterfaces();
 
         #endregion
 
         #region Others
 
-        //services.AddDbContext<AppDbContext>(options =>
-        //    options.UseSqlServer(
-        //        Configuration.GetConnectionString("DefaultConnection"),
-        //        b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
-
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         services.AddScoped<ICurrentUserService, CurrentUserService>();
-        //services.AddTransient<ICurrentUserService, CurrentUserService>();
-        //services.AddTransient<IHttpService, HttpService>();
 
         #endregion
     }
