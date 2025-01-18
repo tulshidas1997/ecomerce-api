1b3d924f94d36228192d8b213483849d5152a3bc
Tulshi Das <37152713+TulshiDas39@users.noreply.github.com>
2025-01-18 15:09:06 +0600

    add product controller

---
+++ Api/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using CleanArchitecture.Core.Dtos;
+using CleanArchitecture.Core.Interfaces.Services;
+using CleanArchitecture.Core.Types;
+using Core.Dtos;
+using Core.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ApiController
+    {
+
+        private readonly IProductService _product;
+
+        public ProductController(IProductService product)
+        {
+            _product = product;
+        }
+        
+        [HttpGet("all")]
+        public async Task<ActionResult<ApiResult<List<ProductDto>>>> GetAll()
+        {
+            var cows = await _product.GetAll();
+            return OkResult(cows);
+        }
+    }
+}
